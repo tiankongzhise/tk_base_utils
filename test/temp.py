8b2dd9400dcbdf51be6af02902096291b608a460
tiankongzhise <104021016+tiankongzhise@users.noreply.github.com>
2025-08-21 17:12:27 +0800

    v0.3.0对logger模块进行了增强 (#3)
    * 新定义了日志级别
    
    * 修正了logger的自定义等级无法获取的问题
    
    * 迭代了logger装饰器的实现,让装饰器可以分层输出日志.
    
    ---------
    
    Co-authored-by: hbc_thinkbook <419337244@qq.com>

--- test/temp.py
+++ test/temp.py
@@ -1,8 +1,28 @@
-from pathlib import Path
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
+
+from tk_base_utils.tk_logger import logger_wrapper,set_logger_config_path,get_logger
+from tk_base_utils import find_file
 
 if __name__ == "__main__":
-    x = Path(__file__)
-    y = Path(x)
-    print(f'x is {x},y is {y}')
+    log_config_path = find_file('test_log_config.toml')
+    set_logger_config_path(log_config_path)
+    logger = get_logger()
+    logger.debug("这是一条debug日志")
+    logger.info_config("这是一条info_config日志")
+    logger.info_utils("这是一条info_utils日志")
+    logger.info_database("这是一条info_database日志")
+    logger.info_kernel("这是一条info_kernel日志")
+    logger.info_core("这是一条info_core日志")
+    logger.info_service("这是一条info_service日志")
+    logger.info_control("这是一条info_control日志")
+    logger.info("这是一条info日志")
+    logger.warning("这是一条warning日志")
+    logger.error("这是一条error日志")
+    logger.critical("这是一条critical日志")
+
+
+
 
 
